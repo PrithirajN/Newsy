47cd4a0a116146b2a430e8e76a51a36d0d064cbb
Prithiraj Nandan <60390549+PrithirajN@users.noreply.github.com>
2020-07-06 22:48:02 +0530

    Uploaded

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAe8k-COgmDWqvmgM046Qeipqx-11URNrs",
+  authDomain: "newsy-4c0fd.firebaseapp.com",
+  databaseURL: "https://newsy-4c0fd.firebaseio.com",
+  projectId: "newsy-4c0fd",
+  storageBucket: "newsy-4c0fd.appspot.com",
+  messagingSenderId: "871330034761",
+  appId: "1:871330034761:web:5d3d43b0e353e32f8b7b98"
+};
+
+export default firebaseConfig;
